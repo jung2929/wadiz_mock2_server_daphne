52ccb583fdc6d3dae2f2b80bdc38698bf5e543ae
You-hyeonyeong <dbgusdud412@naver.com>
2019-11-11 19:55:03 +0900

    배송지 조회

--- src/app/routes/deliveryRoute.js
+++ src/app/routes/deliveryRoute.js
@@ -2,4 +2,5 @@ module.exports = function (app) {
     const delivery = require('../controllers/deliveryController');
     const jwtMiddleware = require('../../../config/jwtMiddleware');
     app.route('/delivery').put(jwtMiddleware, delivery.updateDelivery);
+    app.get('/delivery', jwtMiddleware, delivery.getDelivery);
 }
